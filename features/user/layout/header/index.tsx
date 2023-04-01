598a58436f2125786d8063f26f7bb7364525850d
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2023-04-02 00:42:18 +0545

    feat(auth): register functionality :fire:

--- features/user/layout/header/index.tsx
+++ features/user/layout/header/index.tsx
@@ -1,16 +1,17 @@
 import Logo from '@/shared/components/logo';
 import Search from '@/shared/components/search';
-import React from 'react';
+import React, { FC } from 'react';
 import UserMenu from './user-menu';
+import { User } from '@prisma/client';
 
-const Header = () => {
+const Header: FC<{ currentUser: User | null }> = ({ currentUser }) => {
   return (
     <header className="fixed w-full border-b  z-10 shadow-sm">
       <div className="py-4 container">
         <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
           <Logo></Logo>
           <Search></Search>
-          <UserMenu></UserMenu>
+          <UserMenu {...{ currentUser }}></UserMenu>
         </div>
       </div>
     </header>
